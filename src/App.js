9e2417d58cec1b3e74e4a6f2df270fab815fbb81
Meghna <manu.narwade2810@gmail.com>
2025-01-31 00:46:17 +0530

    Styling JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import CallRequest from './components/CallRequest';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CallRequest />
     </div>
   );
 }
---
+++ src/components/CallRequest.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './myStyles.css'
+
+function CallRequest() {
+  return (
+    <div>
+       <div> <h1 className="primary">List Creation</h1></div>
+        <button className='color-b'>Create a new list</button>
+    </div>
+    
+  )
+}
+
+export default CallRequest
